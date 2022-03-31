045e68166ed096ebe65801636c75a25ee27b082f
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-31 10:00:42 +0530

    [Manoj]Added_Welcome

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object
+            AddressBookSystem addressBook = new AddressBookSystem();
+
+            Console.WriteLine("\n************\nWELCOME TO ADDRESS BOOK PROGRAM\n************\n\n");
+            addressBook.ChooseOption();
+        }
+    }
+}
