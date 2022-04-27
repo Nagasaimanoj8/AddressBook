67970a6fb4ea4313dee1b03f2ff3b75453328d25
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-27 21:52:48 +0530

    [Manoj]adding_DeletingContacts

--- Program.cs
+++ Program.cs
@@ -8,13 +8,14 @@ using System.Threading.Tasks;
 namespace AddressBook2
 {
     class Program
-    {  /// <summary>
-       /// Welcome to Address Book Management System
-       /// UC1 - Gets the contact detail
-       /// UC2- Get and stores multiple contacts
-       /// UC3- Edit the contacts
-       /// </summary>
-       /// <param name="args">The arguments.</param>
+    { /// <summary>
+      /// Welcome to Address Book Management System
+      /// UC1 - Gets the contact detail
+      /// UC2- Get and stores multiple contacts
+      /// UC3- Edit the contacts
+      /// UC4-Deleting the particular contact detail
+      /// </summary>
+      /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address book");
