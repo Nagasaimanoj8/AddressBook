9ee62acbc52fb954cd4abceae5a20021c425bad7
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-27 21:11:50 +0530

    [Manoj}Adding_CreateContacts

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AddressBook2
+{
+    class Edit  // Class Edit
+    {
+        /// <summary>
+        /// Welcome to Address Book Management System
+        /// //UC1-C
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book");
+            //calling operation management class that contains operation
+            OperationManagement.AddDetails();
+            Console.Read();
+        }
+    }
+}
